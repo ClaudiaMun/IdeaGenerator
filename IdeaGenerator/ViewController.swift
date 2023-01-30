ea03d0ebb0bcb55ad308f3914def92f7d97bdd58
ClaudiaMun <claudia.munozcortes@students.dominican.edu>
2023-01-29 17:01:20 -0800

    Initial Commit

---
+++ IdeaGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdeaGenerator
+//
+//  Created by Claudia Munoz on 1/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
